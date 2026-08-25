cd320532b9e126a6c71da641a80e62426daa4147
Muhammad Fahad Baig <fadi@fahadbaig.com>
2026-08-26 01:20:36 +0900

    chore(web): update displayed OSS version to 0.2a

--- apps/web/components/shell/CopyrightFooter.tsx
+++ apps/web/components/shell/CopyrightFooter.tsx
@@ -3,6 +3,6 @@ import MusicWidget from "./MusicWidget";
 import styles from "./CopyrightFooter.module.css";
 
 export default function CopyrightFooter() {
-  const copyright = <div className={styles.notice}><span>© 2026 ECHORA</span><span>MUSIC INTELLIGENCE</span><span>OSS v0.1a</span></div>;
+  const copyright = <div className={styles.notice}><span>© 2026 ECHORA</span><span>MUSIC INTELLIGENCE</span><span>OSS v0.2a</span></div>;
   return <AppFooter marker={null} pinned aside={copyright}><MusicWidget /></AppFooter>;
 }
